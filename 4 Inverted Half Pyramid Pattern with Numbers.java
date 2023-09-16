87a39e823fd120b114a8210f24f601f42be2540a
Rajesh Kumar Mohanta <119045660+Rajesh7853814223@users.noreply.github.com>
2023-09-16 12:15:57 +0530

    Create 4 Inverted Half Pyramid Pattern with Numbers.java

---
+++ 4 Inverted Half Pyramid Pattern with Numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
