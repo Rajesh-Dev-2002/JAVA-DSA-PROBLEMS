d0907ee0c157fce09837ab018782244600a378f4
Rajesh Kumar Mohanta <119045660+Rajesh-Dev-2002@users.noreply.github.com>
2023-10-17 10:27:11 +0530

    Create greatest number in an Arraylist.java

---
+++ greatest number in an Arraylist.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;;
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        // for add the element in the list
+        list.add(5);
+        list.add(7);
+        list.add(3);
+        list.add(9);
+        list.add(6);
+
+        // to find the maximum number in an arraylist
+
+        int max = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (max < list.get(i)) {
+                max = list.get(i);
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
