dff870114ed9f85f83641eeea555defc364879be
Rajesh Kumar Mohanta <119045660+Rajesh-Dev-2002@users.noreply.github.com>
2023-10-17 10:01:28 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;;
+public class arraylist{
+    public static void main(String[] args){
+      ArrayList<Integer> list = new ArrayList<>();
+      // for add the element in the list
+       list.add(5);
+       list.add(7);
+       list.add(3);
+       list.add(9);
+       list.add(6);
+    //    System.out.println(list);
+         //to get the
+    int x = list.get(3);
+    System.out.println(x);
+
+    }
+}
