c61632e165aced7765fcf151aa0f614b80a37b41
Rajesh Kumar Mohanta <119045660+Rajesh-Dev-2002@users.noreply.github.com>
2023-10-18 09:38:26 +0530

    Create Arrays .toString method .java

---
+++ Arrays .toString method .java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class array {
+    public static void main(String[] args){
+        int arr[] ={5,8,9,7,2,3,6};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
